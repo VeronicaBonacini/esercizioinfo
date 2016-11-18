1b4570f9d34d11e2798b5dd56095d7b801b38487
5f03 <5f03@Liceospalla.local>
2016-11-18 12:52:06 +0100

    Aggiunto programma index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 function somma (n1,n2){
 		if (n1 > n2) 
 			return n1 + n2;
@@ -20,4 +20,36 @@ const differenza = (n5, n6) => {
 		
 		return'N5 deve essere minore di N6';
 }
-console.log(differenza(2,10));
+console.log(differenza(2,10));*/
+
+const fs = require('fs');
+
+try {
+	fs.mkdirSync(./persona');
+} catch (err) ();
+
+const persona = {
+	nome: "Veronica",
+	cognome: "Bonacini",
+	anni: 18
+};
+
+const serialize = (jsobject) => {
+	let nome = jsobject.nome;
+	let cognome.jsobject;
+	
+	if (!nome || !cognome) throw new Error("Manca il nome o il cognome della perosona che si vuole salvare") 
+		
+	nome = nome.toLowerCase();
+	cognome = cognome.toLowerCase();
+	
+	const data = JSON.stringify(jsobject);
+	const fileName = '$(nome)_$(cognome).json';
+	
+	fs.writeFile('./'$(fileName), data, (err) => {
+		if (err) throw err;
+		console.log('Persona salvata in $(jsobject)';
+		});
+};
+
+serialize(persona);		
